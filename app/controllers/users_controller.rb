cf43112a10313606f9f004f8576eb3d81cc33707
Johnny Choo <jchoo156@gmail.com>
2016-10-05 16:27:27 -0700

    add username column to user migration

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -30,6 +30,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-  	params.require(:user).permit(:first_name, :last_name, :email, :password_hash, :role)
+  	params.require(:user).permit(:username, :first_name, :last_name, :email, :password_hash, :role)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161005183333) do
+ActiveRecord::Schema.define(version: 20161005230830) do
 
   create_table "users", force: :cascade do |t|
     t.string   "first_name"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20161005183333) do
     t.string   "role"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
+    t.string   "username"
   end
 
 end
