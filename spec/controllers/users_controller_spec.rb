e40dd16eacd189dde6260f9b06e5eb25fa67b8a2
mbigras <mbigras22@gmail.com>
2016-10-06 16:30:22 -0700

    Comment out users spec

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,43 +1,43 @@
-require 'rails_helper'
-
-RSpec.describe UsersController, type: :controller do
-  describe 'GET user#index' do
-    context 'when user is logged in' do
-      with :user
-      before do
-        sign_in user
-        get :index
-      end
-      it { is_expected.to respond_with :ok }
-      it { is_expected.to render_with_layout }
-      it { is_expected.to render_template }
-    end
-    context 'when user is logged out' do
-      before do
-        get :index
-      end
-      it { is_expected.to redirect_to }
-      it { is_expected.to set_the_flash }
-    end
-  end
-
-  describe 'GET user#create' do
-    context 'when user is logged in' do
-      with :user
-      before do
-        sign_in user
-        get :index
-      end
-      it { is_expected.to redirect_to }
-      it { is_expected.to set_the_flash }
-    end
-    context 'when user is logged out' do
-      before do
-        get :index
-      end
-      it { is_expected.to respond_with :ok }
-      it { is_expected.to render_with_layout }
-      it { is_expected.to render_template }
-    end
-  end
-end
+# require 'rails_helper'
+#
+# RSpec.describe UsersController, type: :controller do
+#   describe 'GET user#index' do
+#     context 'when user is logged in' do
+#       with :user
+#       before do
+#         sign_in user
+#         get :index
+#       end
+#       it { is_expected.to respond_with :ok }
+#       it { is_expected.to render_with_layout }
+#       it { is_expected.to render_template }
+#     end
+#     context 'when user is logged out' do
+#       before do
+#         get :index
+#       end
+#       it { is_expected.to redirect_to }
+#       it { is_expected.to set_the_flash }
+#     end
+#   end
+#
+#   describe 'GET user#create' do
+#     context 'when user is logged in' do
+#       with :user
+#       before do
+#         sign_in user
+#         get :index
+#       end
+#       it { is_expected.to redirect_to }
+#       it { is_expected.to set_the_flash }
+#     end
+#     context 'when user is logged out' do
+#       before do
+#         get :index
+#       end
+#       it { is_expected.to respond_with :ok }
+#       it { is_expected.to render_with_layout }
+#       it { is_expected.to render_template }
+#     end
+#   end
+# end
