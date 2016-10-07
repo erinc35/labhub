a991dc24f3715855bbdb606b23918188094f1cec
Johnny Choo <jchoo156@gmail.com>
2016-10-06 23:30:14 -0700

    added comments to experiments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,17 +1,26 @@
 class CommentsController < ApplicationController
   def new
+    p "$"*60
+    p params
     @comment = Comment.new
-    @proposal = Proposal.find(params[:proposal_id])
+    @proposal = Proposal.find(params[:proposal_id]) if params[:proposal_id]
+    @experiment = Experiment.find(params[:experiment_id]) if params[:experiment_id]
   end
 
   def create
-    @proposal = Proposal.find(params[:proposal_id])
-    @comment = @proposal.comments.new(comment_params)
+    @experiment = Experiment.find(params[:experiment_id]) if params[:experiment_id]
+    @proposal = Proposal.find(params[:proposal_id]) if params[:proposal_id]
+
+    @comment = @proposal.comments.new(comment_params) if @proposal
+    @comment = @experiment.comments.new(comment_params) if @experiment
+    
+
     @comment.commentable_type
     @comment.user_id = session[:user_id]
   	
     if @comment.save
-  	  redirect_to proposal_path(@proposal)
+  	  redirect_to proposal_path(@proposal) if @proposal
+      redirect_to experiment_path(@experiment) if @experiment
   	else
   	  render 'new'
   	end
